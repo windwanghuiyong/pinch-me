8f3893650a11a48ba74933cd2fe1e6a479b159f0
王慧勇 <weifeng19911024@163.com>
2017-01-31 21:25:54 +0800

    first commit

--- PinchMe/ViewController.m
+++ PinchMe/ViewController.m
@@ -10,20 +10,66 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIImageView *imageView;
+
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    CGFloat scale, previousScale;
+    CGFloat rotation, previousRotation;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    previousScale = 1;
+    
+    UIImage *image = [UIImage imageNamed:@"yosemite-meadows"];
+    self.imageView = [[UIImageView alloc] initWithImage:image];
+    self.imageView.userInteractionEnabled = YES;		// 开启用户交互(默认关闭)
+    self.imageView.center = self.view.center;
+    [self.view addSubview:self.imageView];
+    
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(doPinch:)];
+    pinchGesture.delegate = self;
+    [self.imageView addGestureRecognizer:pinchGesture];
+    
+    UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(doRotate:)];
+    rotationGesture.delegate = self;
+    [self.imageView addGestureRecognizer:rotationGesture];
 }
 
+#pragma mark - UIGestureRecognizerDelegate
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 两种手势同时识别
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
 }
 
+#pragma mark -
+
+-(void)doPinch:(UIPinchGestureRecognizer *)gesture {
+    scale = gesture.scale;
+    [self transformImageView];
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        previousScale = scale * previousScale;
+        scale = 1;
+    }
+}
+
+- (void)doRotate:(UIRotationGestureRecognizer *)gesture {
+    rotation = gesture.rotation;
+    [self transformImageView];
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        previousRotation = rotation + previousRotation;
+        rotation = 0;
+    }
+}
+
+- (void)transformImageView {
+    CGAffineTransform t = CGAffineTransformMakeScale(scale *previousScale, scale *previousScale);
+    t = CGAffineTransformRotate(t, rotation + previousRotation);
+    self.imageView.transform = t;
+}
 
 @end
---
+++ README.md
@@ -0,0 +1,15 @@
+# swipe
+
+## 描述
+
+1. 捏合和旋转手势
+
+## 知识点
+
+1. 捏合和旋转手势
+
+## 参考
+
+《Beginning iPhone Development with Swift: Exploring the iOS SDK》
+
+《精通iOS开发》第7版 18章 轻点, 触摸, 手势
